99e72253181c8492328d158923ffad008f1b1c24
GemelliAngelo <gemeangelo27@gmail.com>
2025-07-22 11:45:02 +0200

    add style + content

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 function App() {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <div className="card-wrapper">
+        <div className="card">
+          <div className="card-header">
+            <h1 className="card-title">CURRENCY BOOLVERTER</h1>
+          </div>
+          <div className="card-body"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
