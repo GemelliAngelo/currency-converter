c8a9e2e23b0c5c8cacde530609428a64347d07f6
GemelliAngelo <gemeangelo27@gmail.com>
2025-07-22 12:12:30 +0200

    add api fetch + content + style

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
 function App() {
+  function convert(from, to, amount) {
+    fetch(`https://api.frankfurter.dev/v1/latest?base=${from}&symbols=${to}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const convertedAmount = (amount * data.rates[to]).toFixed(2);
+        alert(`${amount} ${from} = ${convertedAmount} ${to}`);
+      });
+  }
   return (
     <div className="container">
       <div className="card-wrapper">
@@ -6,7 +14,36 @@ function App() {
           <div className="card-header">
             <h1 className="card-title">CURRENCY BOOLVERTER</h1>
           </div>
-          <div className="card-body"></div>
+          <div className="card-body">
+            <form>
+              <div className="form-control">
+                <input
+                  type="text"
+                  name="amount"
+                  id="amount"
+                  className="form form-input"
+                />
+                <select
+                  className="form form-select"
+                  name="currencies"
+                  id="currencies"
+                ></select>
+              </div>
+              <div className="form-control">
+                <input
+                  type="text"
+                  name="convertedAmount"
+                  id="convertedAmount"
+                  className="form form-input"
+                />
+                <select
+                  className="form form-select"
+                  name="convertedCurrencies"
+                  id="convertedCurrencies"
+                ></select>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
