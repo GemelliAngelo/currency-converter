c6bbf7bb50c8879c901215fc13a645d627134348
GemelliAngelo <gemeangelo27@gmail.com>
2025-07-26 23:31:52 +0200

    complete + style

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -52,82 +52,46 @@ export default function Graph({ input }) {
         blur: 10,
         opacity: 0.5,
       },
+      toolbar: {
+        show: true,
+        tools: {
+          download: false,
+          zoom: false,
+          pan: false,
+        },
+      },
     },
-    grid: {
-      show: true,
-      borderColor: "#000000",
-      strokeDashArray: 1,
-      position: "back",
-      xaxis: {
-        lines: {
-          show: false,
-        },
-      },
-      yaxis: {
-        lines: {
-          show: true,
-        },
-      },
-      row: {
-        colors: ["#000000"],
-        opacity: 1,
-      },
-      column: {
-        colors: ["#000000"],
-        opacity: 1,
-      },
-      padding: {
-        top: 10,
-        right: 10,
-        bottom: 10,
-        left: 10,
-      },
+    dataLabels: {
+      enabled: false,
     },
     fill: {
-      colors: ["#ff0000ff"],
+      colors: ["#000000"],
       opacity: 1,
       type: "gradient",
       gradient: {
         shade: "dark",
         type: "vertical",
         shadeIntensity: 0.5,
-        gradientToColors: ["#5900ffff"],
+        gradientToColors: ["#ff0000"],
         inverseColors: true,
         opacityFrom: 1,
         opacityTo: 1,
         stops: [0, 100],
       },
     },
-    dataLabels: {
+    stroke: {
+      curve: "smooth",
+      show: true,
+      width: 2,
+      colors: ["#ffffffff"],
+    },
+    tooltip: {
       enabled: true,
-      enabledOnSeries: undefined,
-      textAnchor: "middle",
-      distributed: false,
-      offsetX: 0,
-      offsetY: 0,
       style: {
-        fontSize: "10px",
-        fontFamily: "Helvetica, Arial, sans-serif",
-        fontWeight: "bold",
-        colors: ["#444444"],
+        fontSize: "12px",
       },
-      background: {
-        enabled: true,
-        foreColor: "#ffffffff",
-        padding: 5,
-        borderRadius: 5,
-        borderWidth: 0,
-        borderColor: undefined,
-        opacity: 1,
-        dropShadow: {
-          enabled: true,
-          top: 1,
-          left: 1,
-          blur: 5,
-          color: "#000000",
-          opacity: 1,
-        },
-      },
+      followCursor: true,
+      theme: "dark",
     },
     series: [
       {
